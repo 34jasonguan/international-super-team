6bd2a035522aee1491a5c2e776431c8645e38a54
raul <ren11@duke.edu>
2023-09-09 17:15:06 -0400

    merged

--- recycling_react/src/App.js
+++ recycling_react/src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'; 
-import './App.css'; 
-import Navbar from './components/Navbar'; 
-import Footer from './components/Footer'; 
-import Home from './pages/Home'; 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Navbar />
-      <div className="line">
-      </div>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-      </Routes>
-      <Footer />
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
