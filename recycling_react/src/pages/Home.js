fd6aec12bfd671d2635ea37657a8e2571c27a0a1
34jasonguan <34jasonguan@gmail.com>
2023-09-09 15:54:08 -0400

    Working Navbar and Home Page

---
+++ recycling_react/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home"> 
+        <div className="headerContainer">
+            <n1> Humza likes balls </n1>
+            <p> HE REALLY DOES </p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
