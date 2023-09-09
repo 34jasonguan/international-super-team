4176acf08a28d9e26f617865cd2383335f1428ea
Humza <Humza>
2023-09-09 05:00:28 -0400

    added components + pages, running into dependencies error for route

---
+++ recycling_react/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div classname="top_level">
+        <div classname="title">
+            <h1>Trash2Treasure</h1>
+                <div classname="subtitle">
+                    <p>We Love the Environment</p>
+                </div>
+        </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ recycling_react/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div classname="top_level">
+        <div classname="title">
+            <h1>Trash2Treasure</h1>
+                <div classname="subtitle">
+                    <p>The app to incentivize recycling for duke students</p>
+                </div>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
