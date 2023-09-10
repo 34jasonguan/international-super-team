d2053894012b7cfc70fa7669be6f370134990caa
Humza <Humza>
2023-09-10 06:14:34 -0400

    Submitted starts at top

--- recycling_react/src/pages/Submitted.js
+++ recycling_react/src/pages/Submitted.js
@@ -1,7 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { Component } from 'react';
 import '../styles/Submitted.css'
 
 function Submitted({ currentUser }) {
+    window.scrollTo(0, 0); 
   const [showPoints, setShowPoints] = useState(false);
   const infoUrl = `http://localhost:8080/users/${currentUser}/getPoints`;
   console.log({currentUser});
