da154f61708bc2a6e499a4356ef350f2c84539dd
raul <ren11@duke.edu>
2023-09-09 21:34:03 -0400

    only name

--- recycling_react/src/pages/Rewards.js
+++ recycling_react/src/pages/Rewards.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 
-function Rewards() {
+function Rewards({ currentUser }) {
+
+  const [showPoints, setShowPoints] = useState(false);
+  const infoUrl = `http://localhost:8080/users/${currentUser}/getPoints`;
+
+  // Looks for the data in the backend
+  useEffect(() => {
+      fetch(infoUrl)
+          .then((respose) => respose.json())
+          .then((data) => {
+              // console.log("data", data.name)
+              setShowPoints(data.name)
+          })
+  }, [])
+
   return (
     <div class="rewards">
         <p> Sorry, the rewards page has not been implemented yet. </p>
