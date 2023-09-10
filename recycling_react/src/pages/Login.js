d479ff4b1fef535d6afe06a3763fb46b81b9dcda
34jasonguan <34jasonguan@gmail.com>
2023-09-10 03:05:33 -0400

    Finished About and Login Pages

--- recycling_react/src/pages/Login.js
+++ recycling_react/src/pages/Login.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import sorry from '../images/sorry-login.jpeg'; 
+import '../styles/Login.css'; 
 
 function Login() {
   return (
     <div class="login">
-        <p> Sorry, the login feature has not been implemented yet. </p>
+        <h1>Function Unavailable </h1>
+        <p className="text"> 
+          Unfortunately, due to time restraints and privacy restrictions, we are currently unable to 
+          implement a functional Duke NetID Login system. See the <Link to="/about"><b class="link">About</b></Link> tab for more information. 
+          Please be on the lookout for future updates.  
+        </p>
+        <div className="image">
+          <img src={sorry}></img>
+        </div>
     </div>
   )
 }
