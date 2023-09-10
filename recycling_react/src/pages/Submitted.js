72ede3114b88e5bf74c11e542a76f20771f16ce9
Humza <Humza>
2023-09-10 00:10:44 -0400

    as

---
+++ recycling_react/src/pages/Submitted.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Submitted.css"
+function Submitted() {
+  return (
+    <div className="home"> 
+        <div className="headerContainer">
+            <h1> Congragulations, you now have 10 points!</h1>
+            <p> Your Form has been Submitted </p>
+        </div>
+    </div>
+  )
+}
+
+export default Submitted
